03a17796ccd10697c947f26b32f9512b80d4382e
gjpgabayeron <gjpgabayeron@gmail.com>
2025-11-12 21:49:44 +0800

    init commit

---
+++ src/main/java/pseudopad/Main.java
@@ -0,0 +1,12 @@
+package pseudopad;
+
+/**
+ *
+ * @author Geger John Paul Gabayeron
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
